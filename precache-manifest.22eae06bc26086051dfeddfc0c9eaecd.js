26a35f98929dfc686eb6eb42d2164219048d1c42
Anmol Middha <anmolmiddha2827@gmail.com>
2020-02-23 11:23:14 +0530

    Updates

---
+++ precache-manifest.22eae06bc26086051dfeddfc0c9eaecd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d52833a6717594ac8f436d2ad7ffe07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f58837f34ba630216c31",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "49ec8dee6d42ace87891",
+    "url": "/static/js/2.f39cf76a.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/static/js/2.f39cf76a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f58837f34ba630216c31",
+    "url": "/static/js/main.4b15f09c.chunk.js"
+  },
+  {
+    "revision": "2a20215d102305fff72b",
+    "url": "/static/js/runtime-main.58483d5c.js"
+  }
+]);
